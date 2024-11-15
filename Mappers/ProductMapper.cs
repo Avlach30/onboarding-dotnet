a9c70d74d54e9448114ae6a258d2cbb1535ad1bf
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-15 16:07:11 +0700

    feaT: initialize mapper for module product

---
+++ Mappers/ProductMapper.cs
@@ -0,0 +1,50 @@
+using onboarding_dotnet.Dtos.Products;
+using onboarding_dotnet.Models;
+
+namespace onboarding_dotnet.Mappers;
+
+public static class ProductMapper
+{
+    public static ProductDto ToDto(this Product product)
+    {
+        return new ProductDto
+        {
+            Id = product.Id,
+            CategoryId = product.CategoryId,
+            Name = product.Name,
+            Price = product.Price,
+            Stock = product.Stock,
+            Description = product.Description,
+            Category = product.Category?.ToDto() ?? null,
+            CreatedAt = product.Created_at,
+            UpdatedAt = product.Updated_at
+        };
+    }
+
+    public static ProductResponseDto ToResponse(this Product product)
+    {
+        return new ProductResponseDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Stock = product.Stock,
+            Description = product.Description,
+            Category = product.Category?.ToResponse() ?? null,
+            CreatedAt = product.Created_at,
+            UpdatedAt = product.Updated_at
+        };
+    }
+
+    public static Product ToModel(this ProductDto productDto)
+    {
+        return new Product
+        {
+            CategoryId = productDto.CategoryId,
+            Name = productDto.Name,
+            Price = productDto.Price,
+            Stock = productDto.Stock,
+            Description = productDto.Description
+        };
+    }
+}
