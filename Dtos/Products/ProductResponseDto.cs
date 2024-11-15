c2fdcd87e0cad009fb91296ba79de3b670bf8d5d
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-15 14:24:12 +0700

    feat: initialize dto for product module

---
+++ Dtos/Products/ProductResponseDto.cs
@@ -0,0 +1,22 @@
+using onboarding_dotnet.Dtos.Categories;
+
+namespace onboarding_dotnet.Dtos.Products;
+
+public class ProductResponseDto
+{
+    public int Id { get; set; }
+    
+    public required string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public string? Description { get; set; }
+
+    public CategoryResponseDto? Category { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
