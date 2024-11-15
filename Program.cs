344f6d0f656f16dfd6ddc53e2ce568d6367c19fb
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-15 13:50:42 +0700

    improve: main file

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using onboarding_dotnet.Infrastuctures.Database;
+using onboarding_dotnet.Interfaces.Repositories;
+using onboarding_dotnet.Interfaces.Services;
+using onboarding_dotnet.Repositories;
+using onboarding_dotnet.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UsePathBase(new PathString("/api"));
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
