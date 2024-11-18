c518019c001f5d7b75fcb44790898ce149395645
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-18 11:27:17 +0700

    feat: initialize model and migration for table order products

--- Migrations/20241118042641_CreateOrderProductsTable.Designer.cs
+++ Migrations/20241118042641_CreateOrderProductsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using onboarding_dotnet.Infrastuctures.Database;
 
@@ -11,9 +12,11 @@ using onboarding_dotnet.Infrastuctures.Database;
 namespace onboarding_dotnet.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241118042641_CreateOrderProductsTable")]
+    partial class CreateOrderProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/OrderProduct.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace onboarding_dotnet.Models;
+
+[Table("Order_Products")]
+public class OrderProduct: BaseModel
+{
+    [Column("Order_Id")]
+    public int OrderId { get; set; }
+
+    public Order Order { get; set; } = null!;
+
+    [Column("Product_Id")]
+    public int ProductId { get; set; }
+
+    public Product Product { get; set; } = null!;
+
+    public int Quantity { get; set; }
+}
