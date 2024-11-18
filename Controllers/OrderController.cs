97ce4b80b3e39c6379cd6c8779926093af10a2f2
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-18 17:22:32 +0700

    feat: define endpoint for get one order data by id with relations of products and transaction

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using onboarding_dotnet.Dtos.Orders;
 using onboarding_dotnet.Infrastructures.Responses;
 using onboarding_dotnet.Interfaces.Services;
+using onboarding_dotnet.Mappers;
 
 namespace onboarding_dotnet.Controllers;
 
@@ -15,11 +16,21 @@ public class OrderController(IOrderService orderService): Controller
 
     [HttpPost]
     [Authorize]
-    public async Task<ActionResult<ApiResponse>> CreateOrder([FromBody] OrderRequestDto orderRequestDto)
+    public async Task<ActionResult<ApiResponse>> Store([FromBody] OrderRequestDto orderRequestDto)
     {
         var loggedUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
         await _orderService.CreateAsync(orderRequestDto, loggedUserId);
 
         return Ok(ApiResponse.Success("Order created"));
+    
+    }
+
+    [HttpGet("{id:int}")]
+    [Authorize]
+    public async Task<ActionResult<ApiResponse<OrderResponseDto>>> Show(int id)
+    {
+        var order = await _orderService.GetOne(id, true);
+
+        return Ok(ApiResponse<OrderResponseDto>.Success(order.ToResponse(), "Get order success"));
     }
 }
