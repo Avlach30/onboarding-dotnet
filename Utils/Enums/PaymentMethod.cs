9912ca3e57020af50a7c9d0b8846b143688935eb
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-18 16:10:47 +0700

    feat: define enum for payment method and payment status

---
+++ Utils/Enums/PaymentMethod.cs
@@ -0,0 +1,8 @@
+namespace onboarding_dotnet.Utils.Enums;
+
+public static class PaymentMethod
+{
+    public const string CreditCard = "Credit Card";
+    public const string BankTransfer = "Bank Transfer";
+    public const string EWallet = "E-Wallet";
+}
---
+++ Utils/Enums/PaymentStatus.cs
@@ -0,0 +1,8 @@
+namespace onboarding_dotnet.Utils.Enums;
+
+public static class PaymentStatus
+{
+    public const string Pending = "Pending";
+    public const string Success = "Success";
+    public const string Failed = "Failed";
+}
