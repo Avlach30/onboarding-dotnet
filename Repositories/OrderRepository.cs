e4ff673a117aa216be57e8435ad4411232ceef63
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-18 17:21:03 +0700

    feat: define interface and repository for query logic of order module

---
+++ Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using onboarding_dotnet.Infrastuctures.Database;
+using onboarding_dotnet.Interfaces.Repositories;
+using onboarding_dotnet.Models;
+
+namespace onboarding_dotnet.Repositories;
+
+public class OrderRepository(ApplicationDBContext context): IOrderRepository
+{
+    private readonly ApplicationDBContext _context = context;
+
+    public Task<Order> FindOneWithRelations(int id)
+    {
+        var result = _context.Orders
+            .Include(order => order.User)
+            .Include(order => order.Transaction)
+            .Include(order => order.OrderProducts)
+            .ThenInclude(orderProduct => orderProduct.Product)
+            .AsSplitQuery()
+            .FirstOrDefault(order => order.Id == id) 
+            ?? throw new Exception("Order not found");
+
+        return Task.FromResult(result);
+    }
+
+    public Task<Order> FindOneWithoutRelations(int id)
+    {
+        var result = _context.Orders
+            .FirstOrDefault(order => order.Id == id) 
+            ?? throw new Exception("Order not found");
+
+        return Task.FromResult(result);
+    }
+}
