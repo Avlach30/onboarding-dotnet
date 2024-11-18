4d05f937ce939b1abc7914066e3a7818353d6db8
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-18 16:29:08 +0700

    refactor: enum for payment method

--- Utils/Enums/PaymentMethodType.cs
+++ Utils/Enums/PaymentMethodType.cs
@@ -1,6 +1,6 @@
 namespace onboarding_dotnet.Utils.Enums;
 
-public static class PaymentMethod
+public static class PaymentMethodType
 {
     public const string CreditCard = "Credit Card";
     public const string BankTransfer = "Bank Transfer";
