f7c3fa0557d571de0d789cce591b6e6954f92d82
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-19 16:54:51 +0700

    feat: initialize index request and index response metadata

---
+++ Dtos/Index/IndexRequestDto.cs
@@ -0,0 +1,12 @@
+namespace onboarding_dotnet.Dtos.Index;
+
+public class IndexRequestDto
+{
+    public string? Search { get; set; }
+    public int Page { get; set; } = 1;
+    public int PerPage { get; set; } = 10;
+
+    public string? OrderBy { get; set; }
+
+    public string? OrderDirection { get; set; }
+}
--- Infrastructures/Responses/IndexResponse.cs
+++ Infrastructures/Responses/IndexResponse.cs
@@ -10,13 +10,22 @@ public class IndexResponse<T>
 
     public required List<T> Data { get; set; }
 
-    public static IndexResponse<T> Success(List<T> data, string message = "Ok")
+    public required IndexResponseMetadata Meta { get; set; }
+
+    public static IndexResponse<T> Success(
+        List<T> data, 
+        int total,
+        string message = "Ok",
+        int page = 1,
+        int perPage = 10
+    )
     {
         return new IndexResponse<T>
         {
             Status = (int)HttpStatusCode.OK,
             Message = message,
-            Data = data
+            Data = data,
+            Meta = new IndexResponseMetadata(total, page, perPage)
         };
     }
 }
