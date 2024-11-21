3572ae50fcb67cbb9fba0446f81139ee78c7450f
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-21 17:10:10 +0700

    refactor: implement partial providers into main file

--- Program.cs
+++ Program.cs
@@ -1,113 +1,18 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using onboarding_dotnet.Infrastructures.Mails.Interfaces;
-using onboarding_dotnet.Infrastructures.Mails.Services;
-using onboarding_dotnet.Infrastructures.Policies;
-using onboarding_dotnet.Infrastructures.Schedulers;
-using onboarding_dotnet.Infrastuctures.Database;
-using onboarding_dotnet.Interfaces.Repositories;
-using onboarding_dotnet.Interfaces.Services;
-using onboarding_dotnet.Interfaces.Services.Indexes;
+using onboarding_dotnet.Providers;
 using onboarding_dotnet.Middlewares;
-using onboarding_dotnet.Repositories;
-using onboarding_dotnet.Services;
-using onboarding_dotnet.Utils.Extensions;
-using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add configuration to the container.
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
-// Add Quartz for scheduling jobs
-builder.Services.AddQuartz();
-builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
-builder.Services.AddSingleton<IJob, OrderAutoCancelJob>();
-
-// Add JWT authentication middleware
-builder.Services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options => {
-    {
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
-        };
-    }
-});
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
-        options.JsonSerializerOptions.DictionaryKeyPolicy = new SnakeCaseNamingPolicy();
-    });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Configure Logger
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-builder.Logging.SetMinimumLevel(LogLevel.Information);
-builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
-builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning);
-builder.Logging.AddFilter("System", LogLevel.Warning);
-builder.Logging.AddFilter("onboarding_dotnet", LogLevel.Trace);
-
-builder.Services.AddDbContext<ApplicationDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-builder.Services.AddFluentEMail(builder.Configuration);
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<ICategoryIndexService, CategoryIndexService>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductIndexService, ProductIndexService>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IOrderIndexService, OrderIndexService>();
-
-builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-builder.Services.AddScoped<ITransactionService, TransactionService>();
+var appServiceProvider = new AppServiceProvider(builder);
+appServiceProvider.InitServiceProvider();
+appServiceProvider.InitRepositoryInjection();
+appServiceProvider.InitServiceInjection();
 
 var app = builder.Build();
 
-var schedulerFactory = app.Services.GetRequiredService<ISchedulerFactory>();
-var scheduler = await schedulerFactory.GetScheduler();
-await scheduler.Start();
-
-var jobDetail = JobBuilder.Create<OrderAutoCancelJob>()
-    .WithIdentity("orderAutoCancelJob")
-    .Build();
-
-// Run the job every 1 minute
-var trigger = TriggerBuilder.Create()
-    .WithIdentity("orderAutoCancelJobTrigger")
-    .StartNow()
-    .WithSimpleSchedule(x => x.WithIntervalInMinutes(1).RepeatForever())
-    .Build();
-
-await scheduler.ScheduleJob(jobDetail, trigger);
+var schedulerFactoryProvider = new SchedulerFactoryProvider(app);
+await schedulerFactoryProvider.InitSchedulerFactory();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
