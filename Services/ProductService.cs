5d28a4d3f2d776b8fccfb671f6e6092d342dac9a
avlach19054 <muhammadzakia.19054@mhs.unesa.ac.id>
2024-11-22 15:46:46 +0700

    improve: process update and delete at service layer

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -6,13 +6,9 @@ using onboarding_dotnet.Repositories;
 
 namespace onboarding_dotnet.Services;
 
-public class ProductService(
-    ProductRepository productRepository,
-    CategoryRepository categoryRepository
-)
+public class ProductService(ProductRepository productRepository)
 {
     private readonly ProductRepository _productRepository = productRepository;
-    private readonly CategoryRepository _categoryRepository = categoryRepository;
 
     public async Task<List<Product>> GetAll()
     {
@@ -26,29 +22,19 @@ public class ProductService(
 
     public async Task<AsyncVoidMethodBuilder> Create(ProductRequestDto data)
     {
-        await _categoryRepository.FindOne(data.CategoryId);
-
         return await _productRepository.CreateAsync(data.ToModel());
     }
 
-    public async Task<AsyncVoidMethodBuilder> Update(int id, ProductRequestDto data)
+    public async Task<int> Update(int id, ProductRequestDto data)
     {
-        var product = await _productRepository.FindOne(id);
-        await _categoryRepository.FindOne(data.CategoryId);
+        var updatedData = data.ToModel();
+        updatedData.Id = id;
 
-        product.Name = data.Name;
-        product.Price = data.Price;
-        product.Stock = data.Stock;
-        product.Description = data.Description;
-        product.CategoryId = data.CategoryId;
-
-        return await _productRepository.UpdateAsync(product);
+        return await _productRepository.UpdateAsync(updatedData);
     }
 
-    public async Task<bool> Delete(int id)
+    public async Task<int> Delete(int id)
     {
-        await _productRepository.FindOne(id);
-
         return await _productRepository.Delete(id);
     }
 }
